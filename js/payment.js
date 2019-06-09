9b99360512a3988dbf40f3894f635985f194b1fa
fr1mqa <ivan_emelyanov90@mail.ru>
2019-06-09 13:49:29 +0300

    Payments fix

--- js/payment.js
+++ js/payment.js
@@ -1,11 +1,15 @@
-const shopId = 607074;
-const orderForm = document.querySelector('.products__form');
-const $checkout = YandexCheckoutUI(shopId, {
-    language: 'ru',
-    amount: 3500
+document.addEventListener('DOMContentLoaded', function() {
+    const orderButton = document.querySelector('.products__button');
+    const orderForm = document.querySelector('.products__form');
+    const modalForm = document.querySelector('.modal');
+    const closeModal = document.querySelector('.modal__close');
+    closeModal.addEventListener('click', function () {
+       modalForm.classList.remove('modal__show');
+    });
+    orderButton.addEventListener('click', function () {
+        modalForm.classList.add('modal__show');
+    });
+    orderForm.addEventListener('submit', function (e) {
+       e.preventDefault();
+    });
 });
-const submitHandler = function(e){
-    e.preventDefault();
-    $checkout.open();
-};
-orderForm.addEventListener('submit', submitHandler);
