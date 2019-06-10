760ea0273f4b35f956f5ab34be6db591cefc0f38
Verstak <verstak@leadreaktor.ru>
2019-06-11 00:18:37 +0300

    Payments fix

--- js/payment.js
+++ js/payment.js
@@ -3,13 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const orderForm = document.querySelector('.products__form');
     const modalForm = document.querySelector('.modal');
     const closeModal = document.querySelector('.modal__close');
+    const size = document.querySelector('.products__select');
+    const sizeOrder = document.querySelector('.modal__select');
     closeModal.addEventListener('click', function () {
-       modalForm.classList.remove('modal__show');
+        modalForm.classList.remove('modal__show');
     });
     orderButton.addEventListener('click', function () {
         modalForm.classList.add('modal__show');
     });
     orderForm.addEventListener('submit', function (e) {
-       e.preventDefault();
+        e.preventDefault();
+    });
+    size.addEventListener('change', function () {
+        sizeOrder.selectedIndex = this.selectedIndex;
+    });
+    sizeOrder.addEventListener('change', function () {
+        size.selectedIndex = this.selectedIndex;
     });
 });
